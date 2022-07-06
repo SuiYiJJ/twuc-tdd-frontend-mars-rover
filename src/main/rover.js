eeb6ae894f205b3553a7bc7c0aff844c43de74bc
SuiYiJJ <1184273397@qq.com>
2022-07-06 10:50:43 +0800

    feat: rover can turn right and turn left

--- src/main/rover.js
+++ src/main/rover.js
@@ -12,4 +12,21 @@ export class Rover {
     getPosition() {
         return {x: this.x, y: this.y, direction: this.direction};
     }
+
+    turnLeft() {
+        this.turnDirectionClockwise(false);
+    }
+
+    turnRight() {
+        this.turnDirectionClockwise(true);
+    }
+    turnDirectionClockwise(isClockWise){
+        const clockWise = ['N', 'E', 'S', 'W'];
+        const indexOfClockWise = clockWise.indexOf(this.direction);
+        if(isClockWise){
+            this.direction = this.direction === 'W' ? 'N' : clockWise[indexOfClockWise + 1];
+        }else{
+            this.direction = this.direction === 'N' ? 'W' : clockWise[indexOfClockWise - 1];
+        }
+    }
 }
--- src/test/rover.test.js
+++ src/test/rover.test.js
@@ -11,4 +11,35 @@ describe("Rover", () => {
         // then
         expect(rover.getPosition()).toEqual({x: 1, y: 2, direction: 'N'});
     });
+    it.each(
+        [
+            {initial: 'N', final: 'W'},
+            {initial: 'E', final: 'N'},
+            {initial: 'W', final: 'S'},
+            {initial: 'S', final: 'E'},
+        ]
+    )("should turn left when direction is $initial", ({initial, final}) => {
+        // given
+        const rover = new Rover(1, 2, initial);
+        // when
+        rover.turnLeft();
+        // then
+        expect(rover.getPosition()).toEqual({x: 1, y: 2, direction: final});
+    })
+
+    it.each(
+        [
+            {initial: 'N', final: 'E'},
+            {initial: 'E', final: 'S'},
+            {initial: 'W', final: 'N'},
+            {initial: 'S', final: 'W'},
+        ]
+    )("should turn right when direction is $initial", ({initial, final}) => {
+        // given
+        const rover = new Rover(1, 2, initial);
+        // when
+        rover.turnRight();
+        // then
+        expect(rover.getPosition()).toEqual({x: 1, y: 2, direction: final});
+    })
 })
